06c34c817e1132da2168a1eef55b3a3eb2e95af5
Peter-Georgiev <peter.g.georgiev@gmail.com>
2017-06-26 16:32:43 +0300

    Release 1.01.

--- backup.cs
+++ backup.cs
@@ -3,36 +3,97 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.IO.Compression;
+using System.Threading;
 
 class backup
-{
+{   
     static void Main()
     {
+        //-----------------------------RELEASE 1.01-----------------------------//
         List<string> logMessage = new List<string>();
-
-        logMessage.Add("===============================================================\r\n");
-        logMessage.Add($"Дата и час на архивиране: {DateTime.Now}\r\n");
-
+        
         string[] readConfigFile = new string[0];
         string bacupConf = "backup.conf";
         string dirBackupConf = @"C:\BackupConfAndLog\";
 
         readConfigFile = GetCorrectConfigFile(readConfigFile, bacupConf, dirBackupConf);
 
-        if (!CheckDirectoryAndFile(readConfigFile))
+        byte bacupCount = 0;
+
+        try
         {
-            return;
+             bacupCount = byte.Parse(readConfigFile[7]);
         }
+        catch (Exception)
+        {
+            Console.WriteLine($"Некоректно попълнен файл: \"{bacupConf}\".\r\n" +
+                $"В случай че, незнаете какво да наравите - изтрийте файл: \"{bacupConf}\".\r\n" +
+                $"Файла ще се създаде автоматично с примерно попълнени данни.");            
+        }               
 
         string sourcePath = string.Empty;
         string targetPath = string.Empty;
 
-        if (readConfigFile.Length.Equals(12))
-        {
-            sourcePath = @"" + readConfigFile[1];
-            targetPath = @"" + readConfigFile[3];
+        Console.WriteLine($"...................!!!...МОЛЯ, ИЗЧАКАЙТЕ...!!!...................");
 
-            string[] fileFolderList = Directory.GetFiles(sourcePath, readConfigFile[5]);
+        for (byte count = 0; count < bacupCount; count++)
+        {
+            bool hasExitApp = true;
+            logMessage.Add("===============================================================\r\n");
+            logMessage.Add($"Дата и час на архивиране: {DateTime.Now}\r\n");
+            logMessage.Add("---------------------------------------------------------------\r\n");
+
+            if (!CheckDirectoryAndFile(logMessage, readConfigFile, count))
+            {
+                hasExitApp = false;
+            }
+
+            if (readConfigFile[count * 6 + 13].Equals("*") && hasExitApp)
+            {
+                CopyFolderContents(logMessage, readConfigFile, count);
+            }
+            else if (hasExitApp)
+            {
+                CopyAndZipFile(logMessage, readConfigFile, bacupCount, ref sourcePath, ref targetPath, count);
+            }
+
+            logMessage.Add("---------------------------------------------------------------\r\n");
+            logMessage.Add($"Дата и час на преключване на архива: {DateTime.Now}\r\n");
+            logMessage.Add("===============================================================\r\n");
+            //Test log file - create.
+            string logFile = GetSizeLogFle(readConfigFile, dirBackupConf);
+
+            try
+            {
+                foreach (var item in logMessage)
+                {
+                    File.AppendAllText(logFile, item);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"ВНИМАНИЕ!!!-грешка в пътя до log.txt файла. - Редактирайте: {bacupConf}.");
+                readConfigFile = File.ReadAllLines(bacupConf);
+                Console.Write(string.Join(" ", readConfigFile));
+                return;
+            }
+
+            CopyLogFileToSourcePath(logMessage, readConfigFile, count, logFile);
+
+            CopyLogFileToTargetPath(logMessage, readConfigFile, count, logFile);
+
+            logMessage.Clear();
+        }
+    }
+
+    private static void CopyAndZipFile(List<string> logMessage, string[] readConfigFile, byte bacupCount, ref string sourcePath, ref string targetPath, byte count)
+    {
+        if (readConfigFile.Length.Equals((bacupCount * 6) + 7 + 1))
+        {
+            sourcePath = @"" + readConfigFile[count * 6 + 9];
+            targetPath = @"" + readConfigFile[count * 6 + 11];
+
+            string[] fileFolderList = Directory.GetFiles(sourcePath, readConfigFile[count * 6 + 13]);
 
             foreach (string f in fileFolderList)
             {
@@ -48,8 +109,9 @@ class backup
                     $"Големина на файла за архивиране:" +
                     $" {(double)new FileInfo(f).Length / 1024d / 1024d:F2} MB\r\n");
 
-                if (readConfigFile[9].ToLower().Equals("yes"))
-                {
+                //readConfigFile[3] - zip = Yes;
+                if (readConfigFile[3].ToLower().Equals("yes"))
+                {   //readConfigFile[3] - zip = Yes;
                     DirectoryInfo di = new DirectoryInfo(sourcePath);
                     foreach (FileInfo fi in di.GetFiles())
                     {
@@ -87,8 +149,8 @@ class backup
                         logMessage.Add("ВНИМАНИЕ! - Компресирания файла, не е преместен в отдалечената папка.\r\n");
                     }
                 }
-                else if (readConfigFile[9].ToLower().Equals("no"))
-                {
+                else if (readConfigFile[3].ToLower().Equals("no"))
+                {  
                     sourceFile = Path.Combine(sourcePath, fName);
                     destFile = Path.Combine(targetPath, fName);
 
@@ -110,7 +172,8 @@ class backup
                     isErrase = false;
                 }
 
-                if (readConfigFile[11].ToLower().Equals("yes") && isErrase)
+                //readConfigFile[5] - RemoveOrgFileNameBackup = Yes;
+                if (readConfigFile[5].ToLower().Equals("yes") && isErrase)
                 {
                     try
                     {
@@ -124,36 +187,64 @@ class backup
                 }
             }
         }
+    }
 
-        logMessage.Add("--------------------------------------------------------------\r\n");
-        logMessage.Add($"Дата и час на преключване на архива: {DateTime.Now}\r\n");
-        logMessage.Add("===============================================================\r\n");
-        //Test log file - create.
-        string logFile = GetSizeLogFle(readConfigFile, dirBackupConf);
+    public static void CopyFolderContents(List<string> logMessage, string[] readConfigFile, byte count)
+    {
+        string sourceFolder = @"" + readConfigFile[count * 6 + 9];
+        string targetPath = @"" + readConfigFile[count * 6 + 11];
+
+        logMessage.Add($"Създаване на архивна папка с дата и час в: \"{targetPath}\"\r\n");
+
+        DelayAction(1000);
+
+        string newDir = $"Backup_{DateTime.Now.ToString("dd.MM.yy")}" +
+                        $"_{DateTime.Now.ToString("HH.mm.ss")}";
+
+        string targetFolder = @"" + targetPath + "\\" + newDir;
 
         try
         {
-            foreach (var item in logMessage)
-            {
-                File.AppendAllText(logFile, item);
-            }
+            DirectoryInfo di = Directory.CreateDirectory(targetFolder);
+
+            logMessage.Add($"Успешно създадена архивна папка: \"{targetFolder}\"\r\n");
         }
         catch (Exception)
         {
-            Console.WriteLine($"ВНИМАНИЕ!!!-грешка в пътя до log.txt файла. - Редактирайте: {bacupConf}.");
-            readConfigFile = File.ReadAllLines(bacupConf);
-            Console.Write(string.Join(" ", readConfigFile));
-            return;
+            logMessage.Add($"Проблем при създаване на архивна папка: \"{targetFolder}\"\r\n");
         }
 
-        CopyLogFileToSourcePath(logMessage, readConfigFile, sourcePath, logFile);
+        try
+        {
+            logMessage.Add($"Копиране на папка \"{sourceFolder}\" в \"{targetFolder}\".\r\n");
 
-        CopyLogFileToTargetPath(logMessage, readConfigFile, targetPath, logFile);
+            if (Directory.Exists(sourceFolder))
+            {
+                // Copy folder structure
+                foreach (string sourceSubFolder in Directory.GetDirectories(sourceFolder, "*", SearchOption.AllDirectories))
+                {
+                    Directory.CreateDirectory(sourceSubFolder.Replace(sourceFolder, targetFolder));
+                }
+                // Copy files
+                foreach (string sourceFile in Directory.GetFiles(sourceFolder, "*", SearchOption.AllDirectories))
+                {
+                    string destinationFile = sourceFile.Replace(sourceFolder, targetFolder);
+                    File.Copy(sourceFile, destinationFile, true);
+                }
+            }
+
+            logMessage.Add($"Успешно копиране на папка \"{sourceFolder}\" в \"{targetFolder}\".\r\n");
+        }
+        catch (Exception)
+        {
+            logMessage.Add($"ВНИМАНИЕ! - Папката \"{targetFolder}\" не е копиран в отдалечената папка.\r\n");
+        }
+
     }
 
     private static string GetSizeLogFle(string[] readConfigFile, string dirBackupConf)
-    {
-        string logFile = Path.Combine(dirBackupConf, readConfigFile[7]);
+    {   //readConfigFile[1] - FileLog = log.txt;
+        string logFile = Path.Combine(dirBackupConf, readConfigFile[1]); 
 
         try
         {
@@ -161,7 +252,7 @@ class backup
             {
                 string data = DateTime.Now.ToString("dd.MM.yy");
                 string time = DateTime.Now.ToString("HH.mm");
-                dirBackupConf = Path.Combine(dirBackupConf, data + "_" + time + "_" + readConfigFile[7]);
+                dirBackupConf = Path.Combine(dirBackupConf, data + "_" + time + "_" + readConfigFile[1]);
 
                 File.Move(logFile, dirBackupConf);
             }
@@ -174,51 +265,62 @@ class backup
         return logFile;
     }
 
-    private static void CopyLogFileToTargetPath(List<string> logMessage, string[] readConfigFile, string targetPath, string logFile)
+    private static void CopyLogFileToTargetPath(List<string> logMessage, string[] readConfigFile, byte count, string logFile)
     {
+        string targetPath = @"" + readConfigFile[count * 6 + 11];
+
         try
-        {
-            File.Copy(logFile, Path.Combine(targetPath, readConfigFile[7]), true);
+        {   //readConfigFile[1] - FileLog = log.txt;
+            File.Copy(logFile, Path.Combine(targetPath, readConfigFile[1]), true);
         }
         catch (Exception)
         {
             logMessage.Clear();
 
             File.AppendAllText(logFile, $"===============================================================\r\n");
+            File.AppendAllText(logFile, $"Дата и час на грешката: {DateTime.Now}\r\n");
             File.AppendAllText(logFile, $"{logFile} - не-можа да бъде копиран в \"{targetPath}\" папка.\r\n");
             File.AppendAllText(logFile, $"===============================================================\r\n");
         }
     }
 
-    private static void CopyLogFileToSourcePath(List<string> logMessage, string[] readConfigFile, string sourcePath, string logFile)
+    private static void CopyLogFileToSourcePath(List<string> logMessage, string[] readConfigFile, byte count, string logFile)
     {
+        string sourcePath = @"" + readConfigFile[count * 6 + 9];
+
         try
-        {
-            File.Copy(logFile, Path.Combine(sourcePath, readConfigFile[7]), true);
+        {   //readConfigFile[1] - FileLog = log.txt;
+            File.Copy(logFile, Path.Combine(sourcePath, readConfigFile[1]), true);
         }
         catch (Exception)
         {
             logMessage.Clear();
 
             File.AppendAllText(logFile, $"===============================================================\r\n");
+            File.AppendAllText(logFile, $"Дата и час на грешката: {DateTime.Now}\r\n");
             File.AppendAllText(logFile, $"{logFile} - не-можа да бъде копиран в \"{sourcePath}\" папка.\r\n");
             File.AppendAllText(logFile, $"===============================================================\r\n");
         }
     }
 
-    public static bool CheckDirectoryAndFile(string[] readConfigFile)
+    public static bool CheckDirectoryAndFile(List<string> logMessage, string[] readConfigFile, byte count)
     {
+        string sourcePath = @"" + readConfigFile[count * 6 + 9];
+        string targetPath = @"" + readConfigFile[count * 6 + 11];
+
         bool isExit = true;
 
-        if (!Directory.Exists(readConfigFile[1]))
+        if (!Directory.Exists(sourcePath))
         {
-            Console.WriteLine($"ВНИМАНИЕ! - Директорията \"{readConfigFile[1]}\" не е намерена.");
+            Console.WriteLine($"ВНИМАНИЕ! - Директорията \"{sourcePath}\" не е намерена.");
+            logMessage.Add($"ВНИМАНИЕ! - Директорията \"{sourcePath}\" не е намерена.\r\n");
             isExit = false;
         }
 
-        if (!Directory.Exists(readConfigFile[3]))
+        if (!Directory.Exists(targetPath))
         {
-            Console.WriteLine($"ВНИМАНИЕ! - Директорията \"{readConfigFile[1]}\"не е намерена.");
+            logMessage.Add($"ВНИМАНИЕ! - Директорията \"{targetPath}\" не е намерена.\r\n");
+            Console.WriteLine($"ВНИМАНИЕ! - Директорията \"{targetPath}\" не е намерена.");
             isExit = false;
         }
 
@@ -226,8 +328,8 @@ class backup
     }
 
     public static string GetLogFile(string[] readConfigFile, string dirBackupConf)
-    {        
-        string logFile = Path.Combine(dirBackupConf, readConfigFile[7]);
+    {         //readConfigFile[1] - FileLog = log.txt;
+        string logFile = Path.Combine(dirBackupConf, readConfigFile[1]);
 
         try
         {
@@ -238,6 +340,7 @@ class backup
                 {
                     tw.WriteLine($"===============================================================\r\n" +
                         $"e-mail: peter.g.georgiev@gmail.com\r\n" +
+                        $"Release 1.01\r\n" +
                         $"Create new log file {DateTime.Now}!\r\n" +
                         $"===============================================================\r\n");
                     tw.Close();
@@ -276,10 +379,18 @@ class backup
                 File.Create(path).Dispose();
                 using (TextWriter tw = new StreamWriter(path))
                 {
-                    tw.WriteLine($"SourcePath = C:\\source;" +
-                        $"\r\nTargetPath = D:\\Target;\r\n" +
-                    "FileNameBackup = *.bak;\r\nFileLog = log.txt;" +
-                    "\r\nzip = Yes;\r\nRemoveOrgFileNameBackup = Yes;");
+                    tw.WriteLine(
+                        $"FileLog = log.txt;\r\n" +
+                        $"zip(формат_gz) = Yes;\r\n" +
+                        $"RemoveOrgFileNameBackup(Внимание!) = NO;\r\n" +
+                        $"BackupCount = 2;\r\n" +
+                        $"SourcePath_1 = C:\\1_source;\r\n" +
+                        $"TargetPath_1 = D:\\Target;\r\n" +
+                        $"FileNameBackup_1 = *.bak;\r\n" +
+                        $"SourcePath_2 = C:\\2_source;\r\n" +
+                        $"TargetPath_2 = D:\\Target;\r\n" +
+                        $"FileNameBackup_2 = file.bak;"
+                        );
                     tw.Close();
                 }
             }
@@ -290,7 +401,8 @@ class backup
 
     private static bool GetZipCorrect(string[] configFile)
     {
-        if (configFile[9].ToLower().Equals("yes") || configFile[9].ToLower().Equals("no"))
+        //configFile[3] - zip = yes;
+        if (configFile[3].ToLower().Equals("yes") || configFile[3].ToLower().Equals("no"))
         {
             return true;
         }
@@ -326,5 +438,10 @@ class backup
                 }
             }
         }
+    }
+    
+    public static void DelayAction(int milliseconds)
+    {
+        Thread.Sleep(milliseconds);
     }
 }
